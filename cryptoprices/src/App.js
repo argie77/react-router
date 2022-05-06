8fcbc288f7b847880b3ed63b86ddb5d14b24413a
Brian Krabec <briankrabec@gmail.com>
2022-05-06 13:31:01 -0500

    Nav works but seems like price page not being used

--- cryptoprices/src/App.js
+++ cryptoprices/src/App.js
@@ -4,11 +4,13 @@ import { Route, Routes } from "react-router-dom"
 import Currencies from "./pages/currencies"
 import Main from "./pages/main"
 import Price from "./pages/price"
+import Nav from "./components/nav"
 
 function App() {
   //Use the Route component to specify each route
   return (
     <div className="App">
+      <Nav />
       <Routes>
         <Route path="/" element={<Main />}/>
         <Route path="/currencies" element={<Currencies />}/>
