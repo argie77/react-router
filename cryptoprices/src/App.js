8d32fe64dbb6650362e073a3f5998dd91a2dda9c
Brian Krabec <briankrabec@gmail.com>
2022-05-06 14:14:47 -0500

    Walkthrough complete

--- cryptoprices/src/App.js
+++ cryptoprices/src/App.js
@@ -1,21 +1,12 @@
 import './App.css';
 //IMPORT ROUTE AND COMPONENTS
-import { Route, Routes } from "react-router-dom"
-import Currencies from "./pages/currencies"
-import Main from "./pages/main"
-import Price from "./pages/price"
-import Nav from "./components/nav"
+import { Outlet } from "react-router"
 
 function App() {
   //Use the Route component to specify each route
   return (
     <div className="App">
-      <Nav />
-      <Routes>
-        <Route path="/" element={<Main />}/>
-        <Route path="/currencies" element={<Currencies />}/>
-        <Route path="/price" element={<Price />}/>
-      </Routes>
+      <Outlet />
     </div>
   );
 }
--- cryptoprices/src/index.js
+++ cryptoprices/src/index.js
@@ -3,13 +3,29 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 //IMPORT
-import { BrowserRouter as Router } from "react-router-dom"
+import { BrowserRouter as Router } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Currencies from "./pages/currencies";
+import Price from "./pages/price";
+import Main from "./pages/main";
+import Nav from "./components/nav";
+//Wrap the App Component with the Router component to enable the router features
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Nav />
+      <Routes >
+        <Route path="/" element={<App />} >
+          <Route index element={<Main />} />
+          <Route path="currencies/" element={<Currencies />} />
+          <Route path="price/" element={<Price />} >
+            <Route path=":symbol" element={<Price />} />
+          </Route>
+        </Route>
+      </Routes>
     </Router>
   </React.StrictMode>
 );
