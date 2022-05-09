4c3fb58d7c2a2b1a46c4ef0d38a06b5bca28b3b8
krabecb <60526718+krabecb@users.noreply.github.com>
2022-05-09 00:22:03 -0500

    Update App.js

--- cryptoprices/src/App.js
+++ cryptoprices/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 //IMPORT ROUTE AND COMPONENTS
 //A component that renders the next match in a set of matches
+//In this case, it is Main
+//READ INTO IT: https://reactrouter.com/docs/en/v6/getting-started/concepts
 import { Outlet } from "react-router"
 
 function App() {
